9c58f9972a168bd9c951bac0c40f9ffc79d377e2
Mygod <contact-git@mygod.be>
2025-08-13 00:26:05 -0700

    feat: add parent restrictions

--- packages/config/lib/mutations.js
+++ packages/config/lib/mutations.js
@@ -299,9 +299,10 @@ const applyMutations = (config) => {
   })
 
   config.authentication.areaRestrictions =
-    config.authentication.areaRestrictions.map(({ roles, areas }) => ({
+    config.authentication.areaRestrictions.map(({ roles, areas, parent }) => ({
       roles: roles.flatMap(replaceAliases),
       areas,
+      parent: parent || [],
     }))
 
   config.authentication.strategies = config.authentication.strategies.map(
--- server/src/utils/areaPerms.js
+++ server/src/utils/areaPerms.js
@@ -1,5 +1,7 @@
 // @ts-check
 const config = require('@rm/config')
+const { default: pointInPolygon } = require('@turf/boolean-point-in-polygon')
+const { point } = require('@turf/helpers')
 
 /**
  * @param {string[]} roles
@@ -13,15 +15,63 @@ function areaPerms(roles) {
   for (let i = 0; i < roles.length; i += 1) {
     for (let j = 0; j < areaRestrictions.length; j += 1) {
       if (areaRestrictions[j].roles.includes(roles[i])) {
-        if (areaRestrictions[j].areas.length) {
-          for (let k = 0; k < areaRestrictions[j].areas.length; k += 1) {
-            if (areas.names.has(areaRestrictions[j].areas[k])) {
-              perms.push(areaRestrictions[j].areas[k])
-            } else if (areas.withoutParents[areaRestrictions[j].areas[k]]) {
-              perms.push(...areas.withoutParents[areaRestrictions[j].areas[k]])
+        const restriction = areaRestrictions[j]
+
+        // Handle traditional area restrictions
+        if (restriction.areas && restriction.areas.length) {
+          for (let k = 0; k < restriction.areas.length; k += 1) {
+            if (areas.names.has(restriction.areas[k])) {
+              perms.push(restriction.areas[k])
+            } else if (areas.withoutParents[restriction.areas[k]]) {
+              perms.push(...areas.withoutParents[restriction.areas[k]])
             }
           }
-        } else {
+        }
+
+        // Handle parent-based restrictions
+        if (restriction.parent && restriction.parent.length) {
+          for (let p = 0; p < restriction.parent.length; p += 1) {
+            const parentName = restriction.parent[p]
+
+            // Find all areas that fall within this parent
+            if (areas.polygons[parentName]) {
+              const parentPolygon = areas.polygons[parentName]
+
+              // Check each area to see if it's within the parent
+              areas.names.forEach((areaKey) => {
+                if (areaKey !== parentName && areas.polygons[areaKey]) {
+                  const areaPolygon = areas.polygons[areaKey]
+
+                  // Get a point from the area to test if it's within the parent
+                  let testPoint
+                  if (areaPolygon.type === 'Polygon') {
+                    // Get the first coordinate of the polygon
+                    testPoint = point(areaPolygon.coordinates[0][0])
+                  } else if (areaPolygon.type === 'MultiPolygon') {
+                    // Get the first coordinate of the first polygon
+                    testPoint = point(areaPolygon.coordinates[0][0][0])
+                  }
+
+                  if (testPoint && pointInPolygon(testPoint, parentPolygon)) {
+                    perms.push(areaKey)
+                  }
+                }
+              })
+
+              // Also include the parent itself
+              perms.push(parentName)
+            } else if (areas.withoutParents[parentName]) {
+              // Fallback to the existing parent handling
+              perms.push(...areas.withoutParents[parentName])
+            }
+          }
+        }
+
+        // If neither areas nor parent are specified, allow all areas
+        if (
+          (!restriction.areas || !restriction.areas.length) &&
+          (!restriction.parent || !restriction.parent.length)
+        ) {
           return []
         }
       }
